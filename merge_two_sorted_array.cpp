c3a3f5ac64ad5601ba52ca79dbb3c80eded76ba4
PRANAV RAUT <160023664+PranavRaut1612@users.noreply.github.com>
2024-09-12 20:42:50 +0530

    Add files via upload

---
+++ merge_two_sorted_array.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+void merge(int arr1[], int arr2[], int n, int m) {
+
+    //Declare 2 pointers:
+    int left = n - 1;
+    int right = 0;
+
+    //Swap the elements until arr1[left] is
+    // smaller than arr2[right]:
+    while (left >= 0 && right < m) {
+        if (arr1[left] > arr2[right]) {
+            swap(arr1[left], arr2[right]);
+            left--, right++;
+        }
+        else {
+            break;
+        }
+    }
+
+    // Sort arr1[] and arr2[] individually:
+    sort(arr1, arr1 + n);
+    sort(arr2, arr2 + m);
+}
+
+int main()
+{
+    int arr1[] = {1, 3, 5, 7};
+    int arr2[] = {2, 4, 6, 8};
+    int n = sizeof(arr1)/sizeof(arr1[0]);
+    int m = sizeof(arr2)/sizeof(arr2[0]);
+    merge(arr1, arr2, n, m);
+    cout << "The merged arrays are: " << "\n";
+    cout << "arr1[] = ";
+    for (int i = 0; i < n; i++) {
+        cout << arr1[i] << " ";
+    }
+    cout << "\narr2[] = ";
+    for (int i = 0; i < m; i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
