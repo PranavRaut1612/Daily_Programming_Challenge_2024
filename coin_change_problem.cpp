3ff32b923e47dd097b89858cf1675d1ec8d46cb7
PRANAV RAUT <160023664+PranavRaut1612@users.noreply.github.com>
2024-10-08 22:26:44 +0530

    Add files via upload

---
+++ coin_change_problem.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int coinChange(vector<int>& coins, int amount) {
+   
+    vector<int> dp(amount + 1, INT_MAX);
+    
+   
+    dp[0] = 0;
+    
+    
+    for (int coin : coins) {
+       
+        for (int i = coin; i <= amount; i++) {
+            if (dp[i - coin] != INT_MAX) {  
+                dp[i] = min(dp[i], dp[i - coin] + 1);
+            }
+        }
+    }
+    
+    
+    return dp[amount] == INT_MAX ? -1 : dp[amount];
+}
+
+int main() {
+   
+    vector<int> coins = {1, 2, 5};  
+    int amount = 11;  
+    
+    
+    int result = coinChange(coins, amount);
+    if (result != -1) {
+        cout << "Minimum coins required: " << result << endl;
+    } else {
+        cout << "Amount cannot be formed with the given coins." << endl;
+    }
+    
+    return 0;
+}
